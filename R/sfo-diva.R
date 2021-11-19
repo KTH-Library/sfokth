6c7cac8b2110b0a9ca59ac3b86eb19f2e8378363
Agne <agnel@kth.se>
2021-11-19 16:43:30 +0100

    Functions fetch csv instead of giving url

--- R/sfo-diva.R
+++ R/sfo-diva.R
@@ -1,21 +1,23 @@
-#' Get URL for "csv02" export from DiVA
+#' Get "csv02" export from DiVA
 #'
 #' @param baseurl the base url for the DiVA instance
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
 #' @export
-diva_url_csv02 <- function(baseurl, startyear, stopyear) {
-  sprintf('http://%s/dice/csv02?query=-publicationTypeCode:studentThesis%%20+year:([%d%%20TO%%20%d])&start=0&rows=1000000&sort=author_sort%%20asc', baseurl, startyear, stopyear)
+fetch_diva_csv02 <- function(baseurl, startyear, stopyear) {
+  url <- sprintf('http://%s/dice/csv02?query=-publicationTypeCode:studentThesis%%20+year:([%d%%20TO%%20%d])&start=0&rows=1000000&sort=author_sort%%20asc', baseurl, startyear, stopyear)
+  read.csv(url, fileEncoding = "UTF-8")
+
 }
 
-#' Get URL for "csvall2" export from DiVA
+#' Get "csvall2" export from DiVA
 #'
 #' @param baseurl the base url for the DiVA instance
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
 #' @export
-diva_url_csvall2 <- function(baseurl, startyear, stopyear) {
-  sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[[]]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, startyear, stopyear)
+fetch_diva_csvall2 <- function(baseurl, startyear, stopyear) {
+  url <- sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[[]]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, startyear, stopyear)
+  read.csv(url, fileEncoding = "UTF-8")
 }
-
 
