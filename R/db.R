9b2f46cd85a027fad3e0450c91a99cfdaa6f5bb2
Agne <agnel@kth.se>
2021-11-29 15:37:49 +0100

    Städning

---
+++ R/db.R
@@ -0,0 +1,23 @@
+#' Get connection to Bibmet database server
+#'
+#' @param db the database to connect to, default BIBMET
+#' @param host the host database server, default from Sys.getenv
+#' @param user the database user to connect with, default from Sys.getenv
+#' @param pass the password for the user to connect, default from Sys.getenv
+#' @import odbc
+#' @export
+con_bibmet <- function(db = "BIBMET",
+                       host = Sys.getenv("BIBMET_HOST"),
+                       user = Sys.getenv("BIBMET_USER"),
+                       pass = Sys.getenv("BIBMET_PASS")){
+
+  dbConnect(
+    odbc(),
+    driver = "ODBC Driver 17 for SQL Server",
+    Port = 1433,
+    server = host,
+    database = db,
+    UID = user,
+    PWD = pass,
+    encoding = "Windows-1252")
+}
--- R/sfo-diva.R
+++ R/sfo-diva.R
@@ -22,5 +22,37 @@ fetch_diva_csvall2 <- function(baseurl, startyear, stopyear) {
   url <- sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[[]]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, startyear, stopyear)
   csvall2 <- read.csv(url, encoding = "UTF-8")
   names(csvall2)[1] <- "PID"
-  csvall2 %>% mutate(PID = trimws(PID), Id = tolower(trimws(Id)))
+  csvall2 %>% mutate(PID = trimws(PID))
+}
+
+
+#' Get "csv02" export from DiVA
+#'
+#' @param baseurl the base url for the DiVA instance
+#' @param authors list of authors
+#' @param startyear the first publication year to consider
+#' @param stopyear the last publication year to consider
+#' @import dplyr
+#' @export
+searchauth_diva_csv02 <- function(baseurl, authors, startyear, stopyear) {
+  authq <- paste(paste0('%22', authors, '%22'), collapse = "%20OR%20")
+  url <- sprintf('http://%s/dice/csv02?query=-publicationTypeCode:studentThesis%%20+year:([%d%%20TO%%20%d])+authorId:(%s)&start=0&rows=1000000&sort=author_sort%%20asc', baseurl, startyear, stopyear, authq)
+  csv02 <- read.csv(url, encoding = "UTF-8")
+  names(csv02)[1] <- "PID"
+  csv02 %>% mutate(PID = trimws(PID), Id = tolower(trimws(Id)))
+}
+
+#' Get "csvall2" export from DiVA, by authorId
+#'
+#' @param baseurl the base url for the DiVA instance
+#' @param authors list of authors
+#' @param startyear the first publication year to consider
+#' @param stopyear the last publication year to consider
+#' @export
+searchauth_diva_csvall2 <- function(baseurl, authors, startyear, stopyear) {
+  authq <- paste(glue('[{{"authorId":"{authors}"}}]'), collapse = ",")
+  url <- sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[%s]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, authq, startyear, stopyear)
+  csvall2 <- read.csv(url, encoding = "UTF-8")
+  names(csvall2)[1] <- "PID"
+  csvall2 %>% mutate(PID = trimws(PID))
 }
