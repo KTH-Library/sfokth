11232bee4b8898007050ff7d09a044de5fb14de7
Agne <agnel@kth.se>
2021-11-29 15:40:26 +0100

    Mera städning

---
+++ R/package-sfo.R
@@ -0,0 +1,12 @@
+#' sfo
+#'
+#' The goal of sfo is to provide functionality for retrieving and analyze
+#' data for SFO
+#'
+#' @name sfo
+#' @docType package
+#' @aliases package-svo
+#'
+# needed for use of . in magrittr pipelines
+utils::globalVariables(c("."))
+NULL
