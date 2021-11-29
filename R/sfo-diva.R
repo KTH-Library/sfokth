e13f442470062a8665901f9f8b0db2e5b931b186
Agne <agnel@kth.se>
2021-11-29 15:54:52 +0100

    Städa mera

--- R/sfo-diva.R
+++ R/sfo-diva.R
@@ -4,6 +4,7 @@
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
 #' @import dplyr
+#' @importFrom utils read.csv
 #' @export
 fetch_diva_csv02 <- function(baseurl, startyear, stopyear) {
   url <- sprintf('http://%s/dice/csv02?query=-publicationTypeCode:studentThesis%%20+year:([%d%%20TO%%20%d])&start=0&rows=1000000&sort=author_sort%%20asc', baseurl, startyear, stopyear)
@@ -17,6 +18,8 @@ fetch_diva_csv02 <- function(baseurl, startyear, stopyear) {
 #' @param baseurl the base url for the DiVA instance
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
+#' @import dplyr
+#' @importFrom utils read.csv
 #' @export
 fetch_diva_csvall2 <- function(baseurl, startyear, stopyear) {
   url <- sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[[]]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, startyear, stopyear)
@@ -33,6 +36,7 @@ fetch_diva_csvall2 <- function(baseurl, startyear, stopyear) {
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
 #' @import dplyr
+#' @importFrom utils read.csv
 #' @export
 searchauth_diva_csv02 <- function(baseurl, authors, startyear, stopyear) {
   authq <- paste(paste0('%22', authors, '%22'), collapse = "%20OR%20")
@@ -48,11 +52,15 @@ searchauth_diva_csv02 <- function(baseurl, authors, startyear, stopyear) {
 #' @param authors list of authors
 #' @param startyear the first publication year to consider
 #' @param stopyear the last publication year to consider
+#' @import dplyr
+#' @importFrom utils read.csv
 #' @export
 searchauth_diva_csvall2 <- function(baseurl, authors, startyear, stopyear) {
-  authq <- paste(glue('[{{"authorId":"{authors}"}}]'), collapse = ",")
+  authq <- paste(paste0('[{"authorId":"', authors, '"}]'), collapse = ",")
   url <- sprintf('http://%s/smash/export.jsf?format=csvall2&aq=[%s]&aqe=[]&aq2=[[{"dateIssued":{"from":"%d","to":"%d"}},{"publicationTypeCode":["review","bookReview","monographLicentiateThesis","article","comprehensiveLicentiateThesis","book","manuscript","patent","dissertation","conferenceProceedings","monographDoctoralThesis","report","comprehensiveDoctoralThesis","collection","chapter","conferencePaper","other"]},{"contentTypeCode":["refereed","science","other"]}]]&onlyFullText=false&noOfRows=100000&sortOrder=author_sort_asc', baseurl, authq, startyear, stopyear)
   csvall2 <- read.csv(url, encoding = "UTF-8")
   names(csvall2)[1] <- "PID"
   csvall2 %>% mutate(PID = trimws(PID))
 }
+
+
