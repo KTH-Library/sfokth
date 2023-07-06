23b2a42b7f8ac80f71f0c792b3904f90451e37f0
Agne Larsson <agne.larsson@gmail.com>
2023-07-06 14:06:39 +0200

    Allow for leaving out org in name_address_check()

--- R/checks.R
+++ R/checks.R
@@ -3,26 +3,30 @@
 #' @param con a connection to Bibmet
 #' @param uts list of UT numbers
 #' @param lastname Last name of the person to check
-#' @param org_id Unified_org_id of the org of interest (default 8 = KTH)
+#' @param org_id Unified_org_id of the org of interest, if null no limitation on org
 #'
+#' @import dplyr
 #' @importFrom DBI dbGetQuery
 #'
 #' @export
 
-name_address_check <- function(con, uts, lastname, org_id = 8){
+name_address_check <- function(con, uts, lastname, org_id = NULL){
 
   uts_string <- paste0("'", uts, "'", collapse = ", ")
+
+  org_string <- if_else(is.null(org_id), "", paste("AND bra.Unified_org_id = ", org_id))
 
   q <- sprintf(
   "SELECT bra.UT, auth.Full_name, bra.Full_address
   FROM BIBMET.dbo.BestResAddrAuthorid bra
-  INNER JOIN BIBMET.dbo.Author auth ON (auth.Author_id = bra.Author_id)
+  INNER JOIN BIBMET.dbo.Author auth ON (auth.Author_id = bra.Author_id AND auth.Last_name = '%s')
   WHERE bra.UT IN (%s)
-    AND bra.Unified_org_id = %d
-    AND auth.Last_name = '%s'",
+  %s
+    ",
+  lastname,
   uts_string,
-  org_id,
-  lastname)
+  org_string
+  )
 
   dbGetQuery(con, q)
 }
