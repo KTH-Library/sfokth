489866191f6aa8b53641bb43406dc2e94fba3d12
Agne Larsson <agne.larsson@gmail.com>
2024-01-12 10:19:45 +0100

    Avoid "no global binding" warnings

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,9 @@ globalVariables(
   c("PID", "Id", "p", "cf", "jcf", "size", "OrganisationIds", "StartPage", "EndPage",
     "SeriesISSN", "SeriesEISSN", "ISBN", "ArticleId", "PublicationType", "xvar", "yvar",
     "Publication_Type_WoS", "scop_doctype", "year", "upper", "lower", "interval",
-    "top10", "top20", "site"))
+    "top10", "top20", "site", "C_sciw3", "C_sciwo", "C_scxw3", "C_scxw3_Perc", "C_scxwo", "Cf_scxwo",
+    "Doc_Year", "Doc_type_code_rev", "FRV_sciwo", "FRV_scxw3", "FRV_scxwo", "ISI", "Jcf",
+    "No_addresses", "No_authors", "No_countries", "No_organizations", "Publication_Year",
+    "Publication_year", "Q1", "Q2", "Q3", "ScopusId", "Subj_weights", "Subject_category",
+    "Top10_scxwo", "Top1_scxwo", "Top20", "Top25_scxwo", "Top5_scxwo", "UT", "data",
+    "include_cf", "include_jcf", "isscop", "iswos", "w"))
