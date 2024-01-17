136e56986d3c851ffca87e2787f5a8c4fc3c951f
Agne Larsson <agne.larsson@gmail.com>
2024-01-17 09:21:01 +0100

    Make colors in graphs more manipulable, rename topX_plot -> topXY_plot

--- R/graphs.R
+++ R/graphs.R
@@ -5,11 +5,15 @@
 #' @param timevar name of column with time, default Publication_Year
 #' @param horizontal where to put a horizontal line, default NULL
 #' @param perc set to TRUE for percent scale y axis, default FALSE
+#' @param cols colors to use for curve and horizontal line
 #' @import ktheme dplyr ggplot2 scales
 #' @export
-graph_by_year <- function(df, graphvar, timevar = "Publication_Year", horizontal = NULL, perc = FALSE) {
-
-  kth_cols <- palette_kth(4)
+graph_by_year <- function(df,
+                          graphvar,
+                          timevar = "Publication_Year",
+                          horizontal = NULL,
+                          perc = FALSE,
+                          cols = unname(palette_kth_neo()[c(1, 5)])) {
 
   graphdf <- data.frame(df[, timevar], df[, graphvar]) |>
     rename(xvar = timevar, yvar = graphvar) |>
@@ -33,18 +37,18 @@ graph_by_year <- function(df, graphvar, timevar = "Publication_Year", horizontal
 
   gg <- ggplot(data = graphdf,
          aes(x = xvar, y = yvar, group = 1)) +
-    geom_point() +
-    geom_line(color = kth_cols["blue"], linetype = "dashed") +
+    geom_point(color = cols[1]) +
+    geom_line(color = cols[1], linetype = "dashed") +
     xlab(timevar) +
     ylab(graphvar) +
-    theme_kth_osc() +
+    theme_kth_neo() +
     theme(axis.title.y = element_text(vjust = 2.5),
           panel.grid.major.x = element_blank(),
           panel.grid.minor.y = element_blank()) +
     scale_x_continuous(breaks = breaks, labels = breaks, minor_breaks = NULL)
 
   if(!is.null(horizontal)){
-    gg <- gg + geom_hline(yintercept = horizontal, color = kth_cols["lightblue"])
+    gg <- gg + geom_hline(yintercept = horizontal, color = cols[2])
   }
   if(perc){
     gg <- gg + scale_y_continuous(labels = percent_format(accuracy = 5L), limits = c(0, ymax))
@@ -84,8 +88,8 @@ cf_jcf_plot <- function(df, unfilled = c(), xintercept = 1, yintercept = 1, maxs
     scale_shape_manual(values = shapevals)  +
     scale_size_continuous(range = c(minsize, maxsize), guide = "none") +
     scale_alpha_identity(guide = "none") +
-    geom_vline(xintercept = xintercept, color = kth_colors("red"), linewidth = 1) +
-    geom_hline(yintercept = yintercept, color = kth_colors("red"), linewidth = 1) +
+    geom_vline(xintercept = xintercept, color = pal[4], linewidth = 1) +
+    geom_hline(yintercept = yintercept, color = pal[4], linewidth = 1) +
     scale_x_continuous(limits = c(0, xmax), expand = c(0,0)) +
     scale_y_continuous(limits = c(0, ymax), expand = c(0,0)) +
     theme_light() +
@@ -95,9 +99,15 @@ cf_jcf_plot <- function(df, unfilled = c(), xintercept = 1, yintercept = 1, maxs
     ylab("Jcf value")
 }
 
-#' Plot top10 and jcf by site with circles proportional to number of publications
+#' Plot two top indicators by site
 #'
-#' @param df data frame with indicators by site (needs site, p, top10, top20)
+#' Plots one circle for each site, with size proportional to number of publications
+#' and X/Y coordinates according to top indicators.
+#'
+#' Incoming data frame needs one column \code{site} and two columns \code{topX} and \code{topY}
+#' where \code{X = 100 * xintercept} and \code{Y = 100 * yintercept}.
+#'
+#' @param df data frame with indicators by site
 #' @param unfilled vector of sites to draw unfilled circle for (default empty)
 #' @param xintercept top percentage for publications, default 0.1
 #' @param yintercept top percentage for journals, default 0.2
@@ -106,7 +116,7 @@ cf_jcf_plot <- function(df, unfilled = c(), xintercept = 1, yintercept = 1, maxs
 #' @param solid alpha value for circles, 1 = fully solid, 0 = fully transparent
 #' @import ktheme dplyr ggplot2 scales
 #' @export
-topX_plot <- function(df, unfilled = c(), xintercept = 0.1, yintercept = 0.2, maxsize = 50, pal = palette_kth_neo(), solid = 0.9) {
+topXY_plot <- function(df, unfilled = c(), xintercept = 0.1, yintercept = 0.2, maxsize = 50, pal = palette_kth_neo(), solid = 0.9) {
 
   names(pal) <- NULL
 
@@ -132,8 +142,8 @@ topX_plot <- function(df, unfilled = c(), xintercept = 0.1, yintercept = 0.2, ma
     scale_shape_manual(values = shapevals)  +
     scale_size_continuous(range = c(minsize, maxsize), guide = "none") +
     scale_alpha_identity(guide = "none") +
-    geom_vline(xintercept = xintercept, color = "red", linewidth = 1) +
-    geom_hline(yintercept = yintercept, color = "red", linewidth = 1) +
+    geom_vline(xintercept = xintercept, pal[4], linewidth = 1) +
+    geom_hline(yintercept = yintercept, pal[4], linewidth = 1) +
     scale_x_continuous(limits = c(0, xmax), breaks = xbreaks, labels = percent_format(), expand = c(0,0)) +
     scale_y_continuous(limits = c(0, ymax), breaks = ybreaks, labels = percent_format(), expand = c(0,0)) +
     theme_light() +
